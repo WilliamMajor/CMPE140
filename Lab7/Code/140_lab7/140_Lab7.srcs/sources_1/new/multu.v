357cd5caf90555a4b6bee43b328fda0fe340aab5
William Major <epiphone1995@gmail.com>
2020-04-13 13:32:11 -0700

    small fix

---
+++ Lab7/Code/140_lab7/140_Lab7.srcs/sources_1/new/multu.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/10/2020 09:13:44 PM
+// Design Name: 
+// Module Name: multu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multu(
+        input wire clk,
+        input  wire [31:0] in1,
+        input  wire [31:0] in2,
+        output reg [31:0] hiout,
+        output reg [31:0] lowout
+    );
+        always @ (clk, in1, in2)
+        {hiout,lowout} = in1 * in2;
+    
+    
+    
+    
+    
+endmodule
